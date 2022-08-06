32f1aeb153f881f3188c3487ead81b07f07a93f8
Сергей <sergeyandreev22@gmail.com>
2022-08-07 01:41:50 +0300

    Example 004_Vi4itanie Deployed

---
+++ Example004_Vi4itanie/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Дарова бандит. Говорят ты не можешь посчитать 1 + 1?");
+Console.WriteLine("Ну, так и быть я тебе помогу. Пиши сюда числа, а я посчитаю");
+double Number1 = 31;
+double Number2 = 5;
+double Sum =  Number1 / Number2;
+Console.WriteLine( Sum);
