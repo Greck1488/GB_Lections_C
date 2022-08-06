2f084a8415336a3d007e67e2fe3c8926d321a283
Сергей <sergeyandreev22@gmail.com>
2022-08-07 01:49:48 +0300

    Switch Example003 Number1 and Number2

--- Example003_s4italka/Program.cs
+++ Example003_s4italka/Program.cs
@@ -1,6 +1,11 @@
 ﻿Console.WriteLine("Дарова бандит. Говорят ты не можешь посчитать 1 + 1?");
-Console.WriteLine("Ну, так и быть я тебе помогу. Пиши сюда числа, а я посчитаю");
-int Number1 = 31;
-int Number2 = 5;
+Console.WriteLine("Ну, так и быть я тебе помогу. Смотри, ля как могу");
+int Number1 = new Random().Next(1,10);
+int Number2 = new Random().Next(1,10);
+Console.Write("Первое число ");
+Console.WriteLine(Number1);
+Console.Write("Второе число ");
+Console.WriteLine(Number2);
 int Sum =  Number1 + Number2;
-Console.WriteLine( Sum);
+Console.Write("Сумма ");
+Console.WriteLine(Sum);
