8eff95af67eac6ce3dc97df9f7306997354e7e3a
Сергей <sergeyandreev22@gmail.com>
2022-08-07 01:36:35 +0300

    Example 003 S4italka Deployed

---
+++ Example003_s4italka/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Дарова бандит. Говорят ты не можешь посчитать 1 + 1?");
+Console.WriteLine("Ну, так и быть я тебе помогу. Пиши сюда числа, а я посчитаю");
+int Number1 = 31;
+int Number2 = 5;
+int Sum =  Number1 + Number2;
+Console.WriteLine( Sum);
